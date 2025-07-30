b30649263853b27f41bdc5f0b31cd3defce9f16a
Timur <timur@mail>
2025-07-30 15:53:42 +0900

    null

--- subscribers.rb
+++ subscribers.rb
@@ -1,24 +1,37 @@
 LOGFILE = "subs_log.txt"
 URL = "https://www.youtube.com/@ゆっくりhonebone"
 prev_output = ""
+
 loop do
   time = Time.now
   timestamp = time.strftime("%Y/%m/%d %H:%M:%S")
-  # yt-dlp + jq コマンドを実行し1行だけ取得
+
+  # yt-dlp + jq 実行
   output = %x{ yt-dlp -j "#{URL}" | jq -r 'select(.channel != null) | "\\(.channel) \\(.channel_follower_count) subscribers"' | head -n 1 }.chomp
+
+  # 出力が無ければスキップ
+  if output.nil? || output.strip.empty?
+    puts "#{timestamp} [skip] no output"
+    sleep 60
+    next
+  end
 
   line = "#{timestamp} #{output}"
   puts line
 
-  # ファイルに追記
+  # 変化があったときのみ追記＋git push
   if prev_output != output
-    File.open(LOGFILE, "a:utf-8") do |f|
-      f.puts line
+    File.open(LOGFILE, "a:utf-8") { |f| f.puts line }
+
+    # git操作を非同期で実行
+    Thread.new do
+      system("git add #{LOGFILE}")
+      system("git commit -m 'update at #{timestamp}'")
+      system("git push")
     end
   end
+
   prev_output = output
-  until time + 60 < Time.now
-    sleep 5 
-  end
+  sleep 60  # 1分間待機
 end
 
